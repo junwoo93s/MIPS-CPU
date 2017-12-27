a1441368a4ec7b1759bfcef6c94147cf0160e626
Junwoo Seo <junwoo93s@gmail.com>
2017-12-26 20:48:53 -0600

    Update MUX_ID.v

--- MUX_ID.v
+++ MUX_ID.v
@@ -4,9 +4,9 @@
 // Engineer: 
 // 
 // Create Date: 10/31/2017 01:38:29 PM
-// Design Name: 
+// Design Name: junwoo seo
 // Module Name: MUX_ID
-// Project Name: 
+// Project Name: mips
 // Target Devices: 
 // Tool Versions: 
 // Description: 
