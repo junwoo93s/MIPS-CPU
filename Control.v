9785e1b8a632ce881beae225f35cdb76df1a8011
Junwoo Seo <junwoo93s@gmail.com>
2017-12-26 20:46:38 -0600

    Update Control.v

--- Control.v
+++ Control.v
@@ -4,9 +4,9 @@
 // Engineer: 
 // 
 // Create Date: 10/31/2017 01:29:08 PM
-// Design Name: 
+// Design Name: junwoo seo
 // Module Name: Control
-// Project Name: 
+// Project Name: mips
 // Target Devices: 
 // Tool Versions: 
 // Description: 
