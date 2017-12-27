33a6f27b730572d096ed59181a6eb437a3535fd0
Junwoo Seo <junwoo93s@gmail.com>
2017-12-26 20:47:26 -0600

    Update EXEMEM.v

--- EXEMEM.v
+++ EXEMEM.v
@@ -4,9 +4,9 @@
 // Engineer: 
 // 
 // Create Date: 11/14/2017 01:10:57 PM
-// Design Name: 
+// Design Name: junwoo seo
 // Module Name: EXEMEM
-// Project Name: 
+// Project Name: mips
 // Target Devices: 
 // Tool Versions: 
 // Description: 
