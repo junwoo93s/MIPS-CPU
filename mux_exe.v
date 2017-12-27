50efa31a6d562ab3d5a94f172c4d8be4b84305ac
Junwoo Seo <junwoo93s@gmail.com>
2017-12-26 20:50:23 -0600

    Update mux_exe.v

--- mux_exe.v
+++ mux_exe.v
@@ -4,9 +4,9 @@
     // Engineer: 
     // 
     // Create Date: 11/14/2017 01:10:06 PM
-    // Design Name: 
+    // Design Name: junwoo seo
     // Module Name: mux_exe
-    // Project Name: 
+    // Project Name: mips
     // Target Devices: 
     // Tool Versions: 
     // Description: 
