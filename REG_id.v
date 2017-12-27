622e6b5cadefc2e5b4f21ddf8d77f2f708467f0d
Junwoo Seo <junwoo93s@gmail.com>
2017-12-26 20:49:21 -0600

    Update REG_id.v

--- REG_id.v
+++ REG_id.v
@@ -4,9 +4,9 @@
 // Engineer: 
 // 
 // Create Date: 10/31/2017 01:53:13 PM
-// Design Name: 
+// Design Name: junwoo seo
 // Module Name: REG_id
-// Project Name: 
+// Project Name: mips
 // Target Devices: 
 // Tool Versions: 
 // Description: 
